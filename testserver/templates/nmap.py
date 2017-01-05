256149a6dafd1d21933cfc8c08d903312a3e6b56
Externoak <dariomr1234@gmail.com>
2017-01-05 17:52:50 +0100

    Update nmap.py

--- testserver/templates/nmap.py
+++ testserver/templates/nmap.py
@@ -1,26 +1,26 @@
 <pre>
  #!/usr/bin/env python
-import nmap                         # import nmap.py module
-nm = nmap.PortScanner()             # instantiate nmap.PortScanner object
-nm.scan('127.0.0.1', '1-9999')      # scan host 127.0.0.1, ports from 22 to 443
-nm.command_line()                   # get command line used for the scan : nmap -oX - -p 22-443 127.0.0.1
-nm.scaninfo()                       # get nmap scan informations {'tcp': {'services': '22-443', 'method': 'connect'}}
-nm.all_hosts()                      # get all hosts that were scanned
-nm['127.0.0.1'].hostname()          # get one hostname for host 127.0.0.1, usualy the user record
-nm['127.0.0.1'].hostnames()         # get list of hostnames for host 127.0.0.1 as a list of dict
-                                    # [{'name':'hostname1', 'type':'PTR'}, {'name':'hostname2', 'type':'user'}]
-nm['127.0.0.1'].hostname()          # get hostname for host 127.0.0.1
-nm['127.0.0.1'].state()             # get state of host 127.0.0.1 (up|down|unknown|skipped)
-nm['127.0.0.1'].all_protocols()     # get all scanned protocols ['tcp', 'udp'] in (ip|tcp|udp|sctp)
-nm['127.0.0.1']['tcp'].keys()       # get all ports for tcp protocol
-nm['127.0.0.1'].all_tcp()           # get all ports for tcp protocol (sorted version)
-nm['127.0.0.1'].all_udp()           # get all ports for udp protocol (sorted version)
-nm['127.0.0.1'].all_ip()            # get all ports for ip protocol (sorted version)
-nm['127.0.0.1'].all_sctp()          # get all ports for sctp protocol (sorted version)
-nm['127.0.0.1'].has_tcp(22)         # is there any information for port 22/tcp on host 127.0.0.1
-nm['127.0.0.1']['tcp'][22]          # get infos about port 22 in tcp on host 127.0.0.1
-nm['127.0.0.1'].tcp(22)             # get infos about port 22 in tcp on host 127.0.0.1
-nm['127.0.0.1']['tcp'][22]['state'] # get state of port 22/tcp on host 127.0.0.1 (open
+import nmap                        
+nm = nmap.PortScanner()             
+nm.scan('127.0.0.1', '1-9999')      
+nm.command_line()                   
+nm.scaninfo()                       
+nm.all_hosts()                      
+nm['127.0.0.1'].hostname()          
+nm['127.0.0.1'].hostnames()         
+                                    
+nm['127.0.0.1'].hostname()          
+nm['127.0.0.1'].state()             
+nm['127.0.0.1'].all_protocols()     
+nm['127.0.0.1']['tcp'].keys()       
+nm['127.0.0.1'].all_tcp()           
+nm['127.0.0.1'].all_udp()           
+nm['127.0.0.1'].all_ip()            
+nm['127.0.0.1'].all_sctp()          
+nm['127.0.0.1'].has_tcp(22)         
+nm['127.0.0.1']['tcp'][22]          
+nm['127.0.0.1'].tcp(22)             
+nm['127.0.0.1']['tcp'][22]['state'] 
 
 
 # a more usefull example :
