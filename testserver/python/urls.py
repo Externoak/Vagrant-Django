ce42b71d478e34cc0719b0d216d1afd235691841
Externoak <dariomr1234@gmail.com>
2017-01-05 17:37:09 +0100

    Added download from server and other links

--- testserver/python/urls.py
+++ testserver/python/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^main/$', views.main, name='main'),
     url(r'^interfaces/$', views.main, name='interfaces'),
     url(r'^dependencies/$', views.dependencies, name='dependencies'),
+    url(r'^download/$', views.download, name='download'),
     url(r'^pingcheck/$', views.pingcheck, name='pingcheck'),
     url(r'^webstatus/$', views.webstatus, name='webstatus'),
 ]
--- testserver/python/views.py
+++ testserver/python/views.py
@@ -1,7 +1,9 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.views.generic import RedirectView
+from django.utils.encoding import smart_str
 
 def index(request):    
     return render(request, 'index.html')
@@ -26,3 +28,12 @@ def dependencies(request) :
 
 def interfaces(request) :
     return render(request, 'interfaces.py')
+
+
+def download(request) :
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    path_to_file = os.path.join(BASE_DIR, 'download/Python-Scripts-SYSAdmin.zip')
+    zip_file = open(path_to_file, 'r')
+    response = HttpResponse(zip_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % 'Python-Scripts-SYSAdmin.zip'
+    return response
